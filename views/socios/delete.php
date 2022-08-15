5448cd31c5ef3e87357f51e0368f8c65d4c16fc0
DaliPazmino <dalilapazmino@gmail.com>
2022-08-15 09:44:31 -0500

    dalila

---
+++ views/socios/delete.php
@@ -0,0 +1,49 @@
+<?php include '../template/header.php' ?>
+
+<section class="content">
+    <div>
+       
+        <br><br>
+        <div class="container">
+        <table class="table table-success table-striped">
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                    <th scope="row" id="soc_id">ID</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row" id="soc_nombre">Nombre</th>
+                    <td></td>
+                <tr>
+                    <th scope="row" id="soc_cedula">Cedula</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row" id="soc_direccion">Direccion</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row" id="soc_telefono">Telefono</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row"id="soc_correo">Correo</th>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        <button type="button" class="btn btn-danger"> <img src="../../img/lecho.png" width="30px" heigth="30px"></button>
+        </div>
+        
+      
+
+    </div>
+
+</section>
+<?php include '../template/footer.php' ?>

3103d00bf6318e235c05f78ede7a39f6aa7d2d10
DaliPazmino <dalilapazmino@gmail.com>
2022-08-15 11:15:33 -0500

    actulizacion

--- views/socios/index.php
+++ views/socios/index.php
@@ -1,5 +1,6 @@
 <?php include '../template/header.php' ?>
 
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
@@ -42,6 +43,18 @@
     </tr>
   </thead>
   <tbody>
+    <?php
+       if ($result->num_rows > 0) {
+      
+        while($row = $result->fetch_assoc()) {
+          echo '<tr>';
+          echo  '<th scope="row"> </th>';
+        }
+  
+      } else {
+        echo "0 results";
+      }
+    ?> 
     <tr>
       <th scope="row"> </th>
       <td>  </td>
